4eb4df6c06d49e750abe1db6b678170fb05b7f95
cipher <anf5726@students.uc-bcf.edu.ph>
2024-01-17 11:29:41 +0800

    final

--- c# rampup/Program.cs
+++ c# rampup/Program.cs
@@ -4,24 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+
             // pound to kilogram converter
             Console.WriteLine("pound to kilogram");
             int pound = Convert.ToInt32(Console.ReadLine());
             Double kilogram = pound / 2.2;
             Console.WriteLine(pound + "lb is " + kilogram + "kg");
+            Console.WriteLine();
 
             // mile to kilometer converter
             Console.WriteLine("mile to kilometer");
             int mile = Convert.ToInt32(Console.ReadLine());
             Double kilometer = mile * 1.609;
             Console.WriteLine(mile + "mi is " + kilometer + "km");
+            Console.WriteLine();
 
             // Fahrenheit to Celsius converter
             Console.WriteLine("Fahrenheit to Celsius converter");
             int Fahrenheit = Convert.ToInt32(Console.ReadLine());
             Double Celsius = (Fahrenheit - 32) * 5 / 9;
             Console.WriteLine(Fahrenheit + "°F is " + Celsius + "°c");
+            Console.WriteLine();
 
             // average age of the group
             Console.WriteLine("Group age");
@@ -38,6 +41,7 @@
             double st_average = (st1 + st2 + st3 + st4 + st5 + st6 + st7 + st8 + st9 + st10) / 10;
             Console.WriteLine("The age of the students are" + st1 + st2 + st3 + st4 + st5 + st6 + st7 + st8 + st9 + st10);
             Console.WriteLine("The average of the students is:" + st_average);
+            Console.WriteLine();
 
             //Story
             string a = "ken";
@@ -61,8 +65,65 @@
                 $" {f}, {g}, and the {h}, they finally put an end to {c}. The demon king {c} followed his promise and told them the" +
                 $" way to {e}, and they went on their way. And they also rescued {i}, who was a scholar and knows the exact way to" +
                 $" {e}.");
+            Console.WriteLine();
 
+            //part2
+            Console.WriteLine("Enter a positive integer");
+            int n = Convert.ToInt32(Console.ReadLine());
 
+                if (n <= 0)
+                {
+                    Console.WriteLine("INVALID INPUT");
                 }
+                else
+                {
+                    for (int v = 1; v <= n; v++)
+                        for (int j = 1; j <= v; j++)
+                        {
+                            Console.Write(j + " ");
+                        }
+                    Console.WriteLine();
+                }
+
+            Console.Write("Enter a positive integer: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x <= 0)
+            {
+                Console.WriteLine("Invalid input");
+            }
+            else
+            {
+                int sum = 0;
+
+                for (int y = 1; y <= x; y++)
+                {
+                    sum += y;
+                }
+
+                Console.WriteLine("Output: " + sum);
+            }
+
+            Console.Write("Enter a positive integer: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+                if (z <= 0)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                else
+                {
+                    for (int k = z; k >= 1; k--)
+                    {
+                        for (int j = 1; j <= z; j++)
+                        {
+                            Console.Write(j + " ");
+                        }
+                        Console.WriteLine();
+
+                    }
+                }
+            
+        }
     }
 }
